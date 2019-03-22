1565511973b514144612ea1b7553ed70b1d2b26f
PraveenPillai <PraveenPillai@192.168.0.12>
2019-03-21 22:32:58 -0400

    firebaseapp

--- seleniumday1/src/seleniumday1/TestCase3.java
+++ seleniumday1/src/seleniumday1/TestCase3.java
@@ -1,106 +1,116 @@
 package seleniumday1;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.LogStatus;
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 /*
- * Name : First  test cases using extent reports and access from the excel sheet
- * Description : url-salesforce page
- * 
- * Arguments : path1- path setup for the extent reports
- *          
- * 
- * Created by : Anu Creation date : 19-feb-2019
- *  Last modified date :26-feb-2019
- * 
+ * url passed : https://selenium-prd.firebaseapp.com
+ * Brief Description : Playing with the objects with selenium WebDriver
+ * Created By : Automation team 
+ *  Creation Date : 03/12/2019
+ *  last modified :
  */
-public class TestCase3  extends Test1{
-	
-	public static void main(String args[]) throws  IOException, InterruptedException {
-		TestCase3 test3 = new TestCase3();
-		test3.invokeTestCase1();
-	/*	test3.invokeTestCase2();
-		test3.invokeTestCase3();
-		test3.invokeTestCase4();
-		test3.invokeTestCase5();
-		test3.invokeTestCase1();
-	    test3.invokeTestCase7();
-		test3.invokeTestCase8();
-		test3.invokeTestCase9();
-		test3.invokeTestCase10();  */
-		
-		String path1 = "/Users/PraveenPillai/Desktop/TestcaseReports/sheet1.html";
-		report = new ExtentReports(path1);
-		
-		String dt_path = "/Users/PraveenPillai/Desktop/testcases.xls";
-		String[][] recData = readExceldata(dt_path, "Sheet2");
-
-		for (int i = 0; i < recData.length; i++) {
-			for (int j = 0; j < recData[0].length; j++) {
-
-				System.out.print(recData[i][j] + "  ");
-			}
-			System.out.println();
-		}
-	}
-
-	public static String[][] readExceldata(String dataTablepath, String sheetName) throws IOException {
-		/* Step1: Get the xl path */
-		File xlfile = new File(dataTablepath);
-
-		// Step 2: Access the xl file
-		FileInputStream xlDoc = new FileInputStream(xlfile);
-
-		// Step 3: Access the workbook;
-		HSSFWorkbook wb = new HSSFWorkbook(xlDoc);
-
-		// Step 4: Access the excel sheet
-		HSSFSheet sheet = wb.getSheet(sheetName);
-
-		String val;
-		int iRowCount = sheet.getLastRowNum() + 1;
-		int iColCount = sheet.getRow(0).getLastCellNum();
-		String[][] xlData = new String[iRowCount][iColCount];
-
-		// String val=sheet.getRow(3).getCell(1).getStringCellValue();
-		// System.out.println(val);
-
-		for (int i = 0; i < iRowCount; i++) {
-			for (int j = 0; j < iColCount; j++) {
-				xlData[i][j] = sheet.getRow(i).getCell(j).getStringCellValue();	
-			}
-		}
-		return xlData;
-	}
 
 
+public class TestCase3 {
 
-	private void invokeTestCase1() throws InterruptedException {
-		//String path1 = "/Users/PraveenPillai/Desktop/TestcaseReports/sheet1.html";
-		//report = new ExtentReports(path1);
-		this.initialize();
-		this.launch("https://login.salesforce.com");
-		WebElement uname = driver.findElement(By.id("username"));
-		WebElement pwd = driver.findElement(By.id("password"));
-		WebElement login = driver.findElement(By.id("Login"));
-		logger.log(LogStatus.INFO, " Salesforce application launched");
-		this.enterText(uname, "username", "anu@samplecompany.com");
-		this.enterText(pwd, "password", "pillai123");
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "/Users/PraveenPillai/Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		driver.get("https://selenium-prd.firebaseapp.com/home.html");
+		System.out.println("Application launched successfully");
+		Thread.sleep(2000);
+		WebElement emailUname = driver.findElement(By.id("email_field"));
+		emailUname.clear();
+		emailUname.sendKeys("admin123@gmail.com");
+		WebElement pwd = driver.findElement(By.id("password_field"));
 		pwd.clear();
-		this.selectCheckBox(login, "Login");
-		logger.log(LogStatus.PASS, " Error message : Please enter the password is displayed");
-		this.flushReport();
+		pwd.sendKeys("admin123");
+		WebElement login = driver.findElement(By.xpath("//button[@onclick='login()']"));
+		login.click();
+		Thread.sleep(2000);
+		// Enter name
+		WebElement name = driver.findElement(By.id("name"));
+		name.clear();
+		name.sendKeys("nandu");
+		// Enter fathers name
+		WebElement fatherName = driver.findElement(By.id("lname"));
+		fatherName.clear();
+		fatherName.sendKeys("pillai");
+		// Enter postal address
+		Thread.sleep(2000);
+		WebElement postAdd = driver.findElement(By.id("postaladdress"));
+		postAdd.clear();
+		postAdd.sendKeys("576007");
+		// Enter personal address
+		WebElement perAdd = driver.findElement(By.id("personaladdress"));
+		perAdd.clear();
+		perAdd.sendKeys("bangalore");
+		Thread.sleep(2000);
+		// Radio button
+		WebElement genderButton = driver
+				.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/form[1]/div[5]/span[1]/input[1]"));
+		genderButton.click();
+		Thread.sleep(2000);
+		// Enter city
+		Select city = new Select(driver.findElement(By.name("city")));
+		city.selectByIndex(1);
 
+		// Enter course
+		Select course = new Select(driver.findElement(By.xpath("//select[@id='course']")));
+		course.selectByVisibleText("MBA");
+		Thread.sleep(2000);
+		// Enter district
+		Select dist = new Select(driver.findElement(By.xpath("//select[@id='district']")));
+		dist.selectByVisibleText("GOA");
+		// Enter State
+		Select state = new Select(driver.findElement(By.name("state")));
+		course.selectByVisibleText("PATNA");
+		// Enter pincode
+		WebElement pinCode = driver.findElement(By.id("pincode"));
+		pinCode.clear();
+		pinCode.sendKeys("567007");
+		// Enter email
+		WebElement emailname = driver.findElement(By.id("emailid"));
+		emailname.clear();
+		emailname.sendKeys("abc@gmail.com");
+		Thread.sleep(2000);
+		// Enter Submit button
+		WebElement submit = driver.findElement(By.xpath("//button[@class='bootbutton']"));
+		submit.click();
+		Thread.sleep(2000);
+		// Switch to tab Actions
+		WebElement switchTo = driver.findElement(By.xpath("//button[contains(text(),'Switch To')]  "));
+		switchTo.click();
+		// Click on the alert
+		WebElement alert = driver.findElement(By.xpath("//a[contains(text(),'Alert')]"));
+		alert.click();
+		// click on window
+		WebElement WindowAlert = driver.findElement(By.xpath("//button[contains(text(),'Window Alert')]"));
+		WindowAlert.click();
+		Thread.sleep(2000);
+		Alert WinAlert = driver.switchTo().alert();
+		String alertText = WinAlert.getText();
+		System.out.println("Alert text is : " + alertText);
+		WinAlert.accept();
+		Thread.sleep(2000);
+		// click on prompt alert
+		WebElement promptAlert = driver.findElement(By.xpath("//button[contains(text(),'Promt Alert')]"));
+		promptAlert.click();
+		Thread.sleep(2000);
+		Alert promtselect = driver.switchTo().alert();
+		promtselect.sendKeys("praveen");
+		Thread.sleep(2000);
+		promtselect.accept();
 	}
-	
 
 }
+
+	
